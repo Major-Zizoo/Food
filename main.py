9cba6999a0961ff241da39973fe5a18011796762
Major-Zizoo <norelgndy@gmail.com>
2024-11-17 17:01:09 +0300

    3

---
+++ main.py
@@ -0,0 +1,25 @@
+print("Please select:")
+print("1.Pizza")
+print("2.Burger")
+
+choice=input("Enter option:")
+
+if choice==1:
+    print("Select pizza flavours:")
+    print("1.Margherita Pizza")
+    print("2.Chicken Ranch")
+    pizzachoice=input("Enter option:")
+    if pizzachoice==1 : 
+     print("$20")
+    else:
+     print("$25")
+
+else: 
+    print("Select burger flavours:")
+    print("1.Margherita Pizza")
+    print("2.Chicken Ranch")
+    pizzachoice=input("Enter option:")
+    if pizzachoice==1 : 
+     print("$20")
+    else:
+     print("$25")  
